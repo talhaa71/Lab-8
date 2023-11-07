94dde14bc3c3cd8bb3d8b01aeebe098aa373604c
talhaa71 <143561482+talhaa71@users.noreply.github.com>
2023-11-07 15:28:04 -0600

    added myFunction and correct smoe errors

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function init(){
 //add your javascrip between these two lines of code
  
-  var button = docc=ument.getElementByID("entrybutton");
+  var button = document.getElementById('entrybutton');
   
-  button.addEventListener("click, myFunction")
+  button.addEventListener('click', myFunction);
+  
+  function myFuntion(){
+    var text = document.getElementById('entryinput');
+    text.value += ' after clicking';
+    alert("Talha Ali: ", text.value);
+  }
 
 
 
-
+}
 
 window.addEventListener('load', init);
