88647ac30bb1ebb33906cdd11472cb355f831fda
talhaa71 <143561482+talhaa71@users.noreply.github.com>
2023-11-07 16:04:31 -0600

    made final changes based on final js

--- js/script.js
+++ js/script.js
@@ -13,19 +13,3 @@ function init(){
   }
 
 window.addEventListener('load', init);
-
-/*
-function init(){
-  var button = document.getElementById('entrybutton');
-
-  function showMeText(){
-    var textbox = document.getElementById('entryinput');
-    document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Daniel Krieglstein: " + textbox.value);
-  }
-
-  button.addEventListener('click', showMeText);
-  }
-  
-window.addEventListener('load', init);
-*/
